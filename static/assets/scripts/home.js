68692a252815bf5cff0a73edf82f750dbfa1688f
RagingBro <81316903+RagingBro@users.noreply.github.com>
2024-03-25 10:06:34 -0400

    updated splash texts

--- static/assets/scripts/home.js
+++ static/assets/scripts/home.js
@@ -56,16 +56,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
 })
 
 let splashtext = [
-  'Over 7 Million Users in 2023!',
-  'Fastest growing proxy server!',
-  'Made by Bubbo!',
-  'Check out discord.gg/interstellar :)',
-  'Thanks for using the site',
-  'Follow us on Tiktok (@useinterstellar)',
-  'Subscribe to us on YouTube (@unblocking)',
-  'Subscribe to my Youtube (@xbubbo)',
-  'Check out the settings page',
-  'Check out our Patreon (https://www.patreon.com/gointerstellar)',
+  'why are you reading this lmao',
 ]
 
 document.getElementById('splash').innerText = splashtext[Math.floor(Math.random() * splashtext.length)]
